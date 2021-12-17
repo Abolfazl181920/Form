bc35983354346d5cf7d7369925bb95845e3c72f0
Abolfazl <abolfazlm181920@gmail.com>
2021-12-17 14:43:40 +0330

    delete comments

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ const App = () => {
     lastName: '',
     email: ''
   });
-  
+
   return (
     <div class="form-container">
       <form class="register-form">
-        {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
         <input
           id="first-name"
           class="form-field"
@@ -21,8 +19,6 @@ const App = () => {
           placeholder="First Name"
           name="firstName"
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="first-name-error">Please enter a first name</span> */}
         <input
           id="last-name"
           class="form-field"
@@ -30,8 +26,6 @@ const App = () => {
           placeholder="Last Name"
           name="lastName"
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="last-name-error">Please enter a last name</span> */}
         <input
           id="email"
           class="form-field"
@@ -39,8 +33,6 @@ const App = () => {
           placeholder="Email"
           name="email"
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="email-error">Please enter an email address</span> */}
         <button class="form-field" type="submit">
           Register
         </button>
