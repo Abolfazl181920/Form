6f459667a6e7bebd18c8dd1e8150b063b60295f1
Abolfazl <abolfazlm181920@gmail.com>
2021-12-17 14:28:37 +0330

    add ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
+import React from "react";
+import "./index.css";
+
 const App = () => {
   return (
-    <div className="App"></div>
+    <div class="form-container">
+      <form class="register-form">
+        {/* Uncomment the next line to show the success message */}
+        {/* <div class="success-message">Success! Thank you for registering</div> */}
+        <input
+          id="first-name"
+          class="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="first-name-error">Please enter a first name</span> */}
+        <input
+          id="last-name"
+          class="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="last-name-error">Please enter a last name</span> */}
+        <input
+          id="email"
+          class="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="email-error">Please enter an email address</span> */}
+        <button class="form-field" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
   );
 }
 
